effd31b2f65a738b078601e23924767b12f2c695
denis <denis@morelfr.ch>
2020-08-08 13:34:48 +0200

    Add database Review symlinks construct Remove some bugs

--- xaseco-server/initvolumes.sh
+++ xaseco-server/initvolumes.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
+function symlink() {
+  if [ ! -f "$1" ]; then
+    echo "symlink for $1"
+    ln -s /xaseco/config_files/$(basename $1) $1;
+  fi
+}
+
+# Config files
 if [ "$(ls -A /xaseco/config_files)" ]; then
-     echo "xaseco already setup"
+     echo "xaseco config files already setup"
 else
-    #Copy default file to xasec
-    echo "move all files to new install"
-    mv /xaseco/newinstall/* /xaseco/config_files/
-    echo "touch log files"
-    touch /xaseco/log_files/logfile.txt
-    touch /xaseco/log_files/aseco.log
-    echo "create symlinks"
-    for f in /xaseco/config_files/*.xml;  do ln -s $f /xaseco/$(basename $f); done;
-    for f in /xaseco/config_files/*.php;  do ln -s $f /xaseco/includes/$(basename $f); done;
-    for f in /xaseco/config_files/*.tpl;  do ln -s $f /xaseco/includes/$(basename $f); done;
-    ln -s /xaseco/log_files/logfile.txt /xaseco/logfile.txt
-    ln -s /xaseco/log_files/aseco.log /xaseco/aseco.log
+  #Copy default file to xasec
+  echo "move all files to new install"
+  mv /xaseco/newinstall/* /xaseco/config_files/
 fi
+
+# Log file
+if [ "$(ls -A /xaseco/log_files)" ]; then
+  echo "xaseco log files already setup"
+else
+  echo "touch log files"
+  touch /xaseco/log_files/logfile.txt
+  echo "create symlinks"
+  ln -s /xaseco/log_files/logfile.txt /xaseco/logfile.txt
+fi
+
+#Create symlinks if necessary
+symlink /xaseco/access.xml
+symlink /xaseco/bannedips.xml
+symlink /xaseco/matchsave.xml
+symlink /xaseco/adminops.xml
+symlink /xaseco/config.xml
+symlink /xaseco/musicserver.xml
+symlink /xaseco/rasp.xml
+symlink /xaseco/autotime.xml
+symlink /xaseco/dedimania.xml
+symlink /xaseco/localdatabase.xml
+symlink /xaseco/plugins.xml
+symlink /xaseco/includes/rasp.settings.php
+symlink /xaseco/includes/votes.config.php
+symlink /xaseco/includes/jfreu.config.php
+symlink /xaseco/includes/html.tpl
+symlink /xaseco/includes/text.tpl
